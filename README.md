d269701a9c53064c12b41bd17865bf05bdbbf31b
Michal Soroczynski <soroczynski.m@gmail.com>
2022-06-02 22:57:27 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my Time TRacker REST API
---
+++ script.js
@@ -0,0 +1,10 @@
+const apikey = '';
+const apihost = 'https://todo-api.coderslab.pl';
+
+
+//jedziemy z tym
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+});
